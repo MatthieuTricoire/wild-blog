e2921912ea1c153543d71c719d25c70b735cb437
Matthieu <matthieutricoire@gmail.com>
2024-09-24 16:51:02 +0100

    features: add dummy article data

---
+++ src/dummy_articles.ts
@@ -0,0 +1,17 @@
+export const DUMMY_ARTICLES = [
+  {
+    title: 'Mon premier article',
+    content:
+      'Labore consectetur sint aliqua est ipsum. Culpa commodo duis proident ut duis duis adipisicing fugiat fugiat. Ullamco pariatur duis adipisicing duis exercitation qui occaecat mollit dolor eu proident velit. Occaecat nisi reprehenderit occaecat excepteur voluptate exercitation pariatur mollit ea.',
+  },
+  {
+    title: 'Mon deuxième article',
+    content:
+      'Esse amet pariatur duis incididunt tempor pariatur. Laboris id in velit veniam esse et cupidatat pariatur irure consequat et. Cillum et eu ea non elit adipisicing et dolor Lorem. Do ut in magna cupidatat duis eiusmod ipsum ad culpa ullamco esse commodo dolore. Commodo cillum deserunt adipisicing. Nulla ipsum ad eiusmod velit non occaecat pariatur reprehenderit tempor.',
+  },
+  {
+    title: 'Mon troisième article',
+    content:
+      'Incididunt cupidatat adipisicing Lorem eiusmod elit ex reprehenderit Lorem nostrud culpa reprehenderit magna tempor aliquip mollit. Sunt ad eiusmod eu adipisicing enim qui quis enim ipsum. Mollit consectetur consectetur enim dolore duis. Esse commodo aliqua aliquip nostrud reprehenderit elit esse. Irure tempor elit excepteur ipsum magna. Voluptate excepteur laborum amet adipisicing cupidatat aliqua esse. Minim est magna nisi excepteur officia exercitation duis ipsum fugiat culpa.',
+  },
+];
