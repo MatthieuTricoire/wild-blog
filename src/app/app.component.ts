f2eb7611246f17960664ffaee3651c9517b03720
Matthieu <matthieutricoire@gmail.com>
2024-09-25 23:13:07 +0100

    feature: add new articles data and a function to know if at least one article i published

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,28 @@ import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './shared/card/card.component';
 import { ArticleComponent } from './articles/article/article.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { DUMMY_ARTICLES_DIRECTIVES_QUEST } from '../dummy_articles';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, ArticleComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    CardComponent,
+    ArticleComponent,
+    HeaderComponent,
+    ArticlesComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Bienvenue sur le Wild Blog de Matthieu !';
+  articles = DUMMY_ARTICLES_DIRECTIVES_QUEST;
+
+  isAnyArticlePublished = this.articles.some((a) => a.isPublished === true);
+
 }
