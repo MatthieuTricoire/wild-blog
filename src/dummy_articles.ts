c79456f10ccd35289058779ca1f92f91c96a9e5f
Matthieu <matthieutricoire@gmail.com>
2024-09-25 14:28:02 +0100

    feature: add author, img, isPublished fields

--- src/dummy_articles.ts
+++ src/dummy_articles.ts
@@ -3,15 +3,24 @@ export const DUMMY_ARTICLES = [
     title: 'Mon premier article',
     content:
       'Labore consectetur sint aliqua est ipsum. Culpa commodo duis proident ut duis duis adipisicing fugiat fugiat. Ullamco pariatur duis adipisicing duis exercitation qui occaecat mollit dolor eu proident velit. Occaecat nisi reprehenderit occaecat excepteur voluptate exercitation pariatur mollit ea.',
+    author: 'Reichel LLC',
+    img: 'https://loremflickr.com/640/480',
+    isPublished: false,
   },
   {
     title: 'Mon deuxième article',
     content:
       'Esse amet pariatur duis incididunt tempor pariatur. Laboris id in velit veniam esse et cupidatat pariatur irure consequat et. Cillum et eu ea non elit adipisicing et dolor Lorem. Do ut in magna cupidatat duis eiusmod ipsum ad culpa ullamco esse commodo dolore. Commodo cillum deserunt adipisicing. Nulla ipsum ad eiusmod velit non occaecat pariatur reprehenderit tempor.',
+    author: 'Aufderhar - Kerluke',
+    img: 'https://loremflickr.com/640/480/sports',
+    isPublished: false,
   },
   {
     title: 'Mon troisième article',
     content:
       'Incididunt cupidatat adipisicing Lorem eiusmod elit ex reprehenderit Lorem nostrud culpa reprehenderit magna tempor aliquip mollit. Sunt ad eiusmod eu adipisicing enim qui quis enim ipsum. Mollit consectetur consectetur enim dolore duis. Esse commodo aliqua aliquip nostrud reprehenderit elit esse. Irure tempor elit excepteur ipsum magna. Voluptate excepteur laborum amet adipisicing cupidatat aliqua esse. Minim est magna nisi excepteur officia exercitation duis ipsum fugiat culpa.',
+    author: 'Koepp, Prosacco and Fisher',
+    img: 'https://loremflickr.com/640/480/nature',
+    isPublished: false,
   },
 ];
