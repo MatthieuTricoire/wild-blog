4cbb6694918094bc76413417bdfaee7626501526
Matthieu <matthieutricoire@gmail.com>
2024-09-25 14:29:22 +0100

    feature: improve article component with a toggle visibility and a comment form

--- src/app/articles/article/article.component.ts
+++ src/app/articles/article/article.component.ts
@@ -1,12 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { Article } from '../../../types';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss',
 })
 export class ArticleComponent {
-  @Input({ required: true }) article!: { title: string; content: string };
+  @Input({ required: true }) article!: Article;
+  comments: string[] = [];
+  enteredComment: string = '';
+
+  togglePublication() {
+    this.article.isPublished = !this.article.isPublished;
+  }
+
+  onSubmit() {
+    if (this.enteredComment.length > 0) {
+      this.comments?.push(this.enteredComment);
+    }
+    this.enteredComment = '';
+  }
 }
