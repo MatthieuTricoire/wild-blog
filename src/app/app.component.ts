1c5168fe966fe4c8099bd249537d3a59be99279c
Matthieu <matthieutricoire@gmail.com>
2024-09-25 08:02:05 +0100

    feature: add missing import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './shared/card/card.component';
+import { ArticleComponent } from './articles/article/article.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, ArticleComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Bievenue sur le Wild Blog de Matthieu !';
+  title = 'Bienvenue sur le Wild Blog de Matthieu !';
 }
