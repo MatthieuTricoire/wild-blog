aebb83de3718fe80d6b410a88b717000b65a4e19
Matthieu <matthieutricoire@gmail.com>
2024-09-30 17:17:16 +0200

    feature: add id to articles

--- src/dummy_articles.ts
+++ src/dummy_articles.ts
@@ -1,5 +1,6 @@
 export const DUMMY_ARTICLES = [
   {
+    id: 1,
     title: 'Mon premier article',
     content:
       'Labore consectetur sint aliqua est ipsum. Culpa commodo duis proident ut duis duis adipisicing fugiat fugiat. Ullamco pariatur duis adipisicing duis exercitation qui occaecat mollit dolor eu proident velit. Occaecat nisi reprehenderit occaecat excepteur voluptate exercitation pariatur mollit ea.',
@@ -8,6 +9,7 @@ export const DUMMY_ARTICLES = [
     isPublished: false,
   },
   {
+    id: 2,
     title: 'Mon deuxième article',
     content:
       'Esse amet pariatur duis incididunt tempor pariatur. Laboris id in velit veniam esse et cupidatat pariatur irure consequat et. Cillum et eu ea non elit adipisicing et dolor Lorem. Do ut in magna cupidatat duis eiusmod ipsum ad culpa ullamco esse commodo dolore. Commodo cillum deserunt adipisicing. Nulla ipsum ad eiusmod velit non occaecat pariatur reprehenderit tempor.',
@@ -16,6 +18,7 @@ export const DUMMY_ARTICLES = [
     isPublished: false,
   },
   {
+    id: 3,
     title: 'Mon troisième article',
     content:
       'Incididunt cupidatat adipisicing Lorem eiusmod elit ex reprehenderit Lorem nostrud culpa reprehenderit magna tempor aliquip mollit. Sunt ad eiusmod eu adipisicing enim qui quis enim ipsum. Mollit consectetur consectetur enim dolore duis. Esse commodo aliqua aliquip nostrud reprehenderit elit esse. Irure tempor elit excepteur ipsum magna. Voluptate excepteur laborum amet adipisicing cupidatat aliqua esse. Minim est magna nisi excepteur officia exercitation duis ipsum fugiat culpa.',
@@ -27,6 +30,7 @@ export const DUMMY_ARTICLES = [
 
 export const DUMMY_ARTICLES_DIRECTIVES_QUEST = [
   {
+    id: 1,
     title: 'Angular 16: Les nouveautés',
     author: 'Alice',
     content: "Les nouveautés d'Angular 16 incluent...",
@@ -36,6 +40,7 @@ export const DUMMY_ARTICLES_DIRECTIVES_QUEST = [
     likes: 120,
   },
   {
+    id: 2,
     title: 'Développer une API REST',
     author: 'Bob',
     content: 'Développer une API REST nécessite...',
@@ -45,6 +50,7 @@ export const DUMMY_ARTICLES_DIRECTIVES_QUEST = [
     likes: 75,
   },
   {
+    id: 3,
     title: 'Pourquoi TypeScript est essentiel ?',
     author: 'Charlie',
     content: 'TypeScript apporte de la robustesse...',
@@ -54,6 +60,7 @@ export const DUMMY_ARTICLES_DIRECTIVES_QUEST = [
     likes: 101,
   },
   {
+    id: 4,
     title: 'Pourquoi TypeScript est essentiel ?',
     author: 'Charlie',
     content: 'TypeScript apporte de la robustesse...',
@@ -63,6 +70,7 @@ export const DUMMY_ARTICLES_DIRECTIVES_QUEST = [
     likes: 200,
   },
   {
+    id: 5,
     title: 'Pourquoi TypeScript est essentiel ?',
     author: 'Charlie',
     content: 'TypeScript apporte de la robustesse...',
