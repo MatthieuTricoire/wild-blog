d76699b8ebdea86e78eb72104162ebfb1f3e8e08
Matthieu <matthieutricoire@gmail.com>
2024-09-30 17:16:46 +0200

    create a homepage page

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { DUMMY_ARTICLES_DIRECTIVES_QUEST } from '../../../dummy_articles';
+import { CardComponent } from '../../shared/card/card.component';
+import { ArticleComponent } from '../../articles/article/article.component';
+import { ArticlesComponent } from '../../articles/articles.component';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [
+    CardComponent,
+    ArticleComponent,
+    ArticlesComponent,
+    CommonModule,
+    RouterLink,
+  ],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+})
+export class HomePageComponent {
+  router: Router = inject(Router);
+  title = 'Bienvenue sur le Wild Blog de Matthieu !';
+  articles = DUMMY_ARTICLES_DIRECTIVES_QUEST;
+
+  isAnyArticlePublished = this.articles.some((a) => a.isPublished === true);
+
+  goTo(id: number) {
+    this.router.navigate(['/article/', id]);
+  }
+}
