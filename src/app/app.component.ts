6ef96058a141f58376edf449a6f3d037bb6b06df
Matthieu <matthieutricoire@gmail.com>
2024-09-30 17:18:49 +0200

    feature: move code to homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from './shared/card/card.component';
-import { ArticleComponent } from './articles/article/article.component';
 import { HeaderComponent } from './header/header.component';
-import { ArticlesComponent } from './articles/articles.component';
-import { DUMMY_ARTICLES_DIRECTIVES_QUEST } from '../dummy_articles';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CardComponent,
-    ArticleComponent,
-    HeaderComponent,
-    ArticlesComponent,
-    CommonModule,
-  ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Bienvenue sur le Wild Blog de Matthieu !';
-  articles = DUMMY_ARTICLES_DIRECTIVES_QUEST;
-
-  isAnyArticlePublished = this.articles.some((a) => a.isPublished === true);
-
-}
+export class AppComponent {}
