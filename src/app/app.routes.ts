f65044d2afb39e7afcb296e1d883dc2849c7042e
Matthieu <matthieutricoire@gmail.com>
2024-09-30 17:18:22 +0200

    feature: add home, article, notfound to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  { path: 'article/:id', component: ArticleComponent },
+  { path: '**', component: NotFoundComponent },
+];
