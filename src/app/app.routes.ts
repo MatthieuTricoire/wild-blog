a82fb4c6e84eeb98725043cfabd42786bf5c3b00
Matthieu <matthieutricoire@gmail.com>
2024-10-02 14:31:41 +0200

    feat: add new route to signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ArticleComponent } from './pages/article/article.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
@@ -9,5 +10,6 @@ export const routes: Routes = [
     component: HomePageComponent,
   },
   { path: 'article/:id', component: ArticleComponent },
+  { path: 'signup', component: SignUpComponent },
   { path: '**', component: NotFoundComponent },
 ];
