be70edc02208a9af106e7978e43983af605a70c8
Matthieu <matthieutricoire@gmail.com>
2024-10-03 15:22:34 +0200

    feat: change images used in article

--- src/dummy_articles.ts
+++ src/dummy_articles.ts
@@ -5,7 +5,7 @@ export const DUMMY_ARTICLES = [
     content:
       'Labore consectetur sint aliqua est ipsum. Culpa commodo duis proident ut duis duis adipisicing fugiat fugiat. Ullamco pariatur duis adipisicing duis exercitation qui occaecat mollit dolor eu proident velit. Occaecat nisi reprehenderit occaecat excepteur voluptate exercitation pariatur mollit ea.',
     author: 'Reichel LLC',
-    img: 'https://loremflickr.com/640/480',
+    img: 'https://placehold.co/400',
     isPublished: false,
   },
   {
@@ -14,7 +14,7 @@ export const DUMMY_ARTICLES = [
     content:
       'Esse amet pariatur duis incididunt tempor pariatur. Laboris id in velit veniam esse et cupidatat pariatur irure consequat et. Cillum et eu ea non elit adipisicing et dolor Lorem. Do ut in magna cupidatat duis eiusmod ipsum ad culpa ullamco esse commodo dolore. Commodo cillum deserunt adipisicing. Nulla ipsum ad eiusmod velit non occaecat pariatur reprehenderit tempor.',
     author: 'Aufderhar - Kerluke',
-    img: 'https://loremflickr.com/640/480/sports',
+    img: 'https://placehold.co/400',
     isPublished: false,
   },
   {
@@ -23,7 +23,7 @@ export const DUMMY_ARTICLES = [
     content:
       'Incididunt cupidatat adipisicing Lorem eiusmod elit ex reprehenderit Lorem nostrud culpa reprehenderit magna tempor aliquip mollit. Sunt ad eiusmod eu adipisicing enim qui quis enim ipsum. Mollit consectetur consectetur enim dolore duis. Esse commodo aliqua aliquip nostrud reprehenderit elit esse. Irure tempor elit excepteur ipsum magna. Voluptate excepteur laborum amet adipisicing cupidatat aliqua esse. Minim est magna nisi excepteur officia exercitation duis ipsum fugiat culpa.',
     author: 'Koepp, Prosacco and Fisher',
-    img: 'https://loremflickr.com/640/480/nature',
+    img: 'https://placehold.co/400',
     isPublished: false,
   },
 ];
