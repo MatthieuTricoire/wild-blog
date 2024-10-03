f5fced6669893ec65f0ff70ad9aefe4dd071584b
Matthieu <matthieutricoire@gmail.com>
2024-10-03 15:22:52 +0200

    feat: add signin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ArticleComponent } from './pages/article/article.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,6 @@ export const routes: Routes = [
   },
   { path: 'article/:id', component: ArticleComponent },
   { path: 'signup', component: SignUpComponent },
+  { path: 'signin', component: SignInComponent },
   { path: '**', component: NotFoundComponent },
 ];
