60cfab868b993a3fd43ca6ce2dd948cfa1eafa4a
Matthieu <matthieutricoire@gmail.com>
2024-10-03 15:21:22 +0200

    feat: move header to components folder

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { ArticlesComponent } from '../articles/articles.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ArticlesComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
